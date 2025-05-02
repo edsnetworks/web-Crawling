ea746fae9d4d118613e9b5f66a9c241500305d8d
giuseppe de santis <31151230+edsnetworks@users.noreply.github.com>
2025-05-02 13:16:40 +0200

    Update crawl4ai.sh

--- crawl4ai.sh
+++ crawl4ai.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Configuration
 OUTPUT_DIR="./output"
-CRAWL4AI_URL="http://your-server-ip:11235/"
+CRAWL4AI_URL="http://your-server-ip:11235/"  #replace with your server IP or in the same machine use localhost or host.docker.internal
 SLEEP_TIME=1  # Seconds to wait between requests
 # --- Functions ---
 urlencode() {
